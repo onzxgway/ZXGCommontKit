f6a7c82ea8d162035f8420d31c9b563a8ed138c8
zhuxianguo529@163.com <zhuxianguo529@163.com>
2018-05-19 13:39:57 +0800

    add

--- ZXGCommonKit/Classes/CustomView/ZXGLayoutButton.h
+++ ZXGCommonKit/Classes/CustomView/ZXGLayoutButton.h
@@ -7,8 +7,23 @@
 
 #import <UIKit/UIKit.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
+typedef NS_ENUM(NSUInteger, ZXGLayoutButtonType) {
+    ZXGLayoutButtonTypeImageLeftTitleRight,
+    ZXGLayoutButtonTypeImageRightTitleLeft,
+    ZXGLayoutButtonTypeImageTopTitleBottom,
+    ZXGLayoutButtonTypeImageBottomTitleTop,
+};
+
 @interface ZXGLayoutButton : UIButton
 
+// 布局方式，默认值ZXGLayoutButtonTypeImageLeftTitleRight
+@property (nonatomic) ZXGLayoutButtonType layoutType;
 
+// 图片和文字的间距，默认值8
+@property (nonatomic) CGFloat margin;
 
 @end
+
+NS_ASSUME_NONNULL_END
--- ZXGCommonKit/Classes/CustomView/ZXGLayoutButton.m
+++ ZXGCommonKit/Classes/CustomView/ZXGLayoutButton.m
@@ -9,12 +9,85 @@
 
 @implementation ZXGLayoutButton
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+#pragma mark - LifeCycle
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        [self prepare];
+    }
+    return self;
 }
-*/
+
+- (nullable instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:coder];
+    if (self) {
+        [self prepare];
+    }
+    return self;
+}
+
+
+- (void)layoutSubviews {
+    [super layoutSubviews];
+    
+    [self.imageView sizeToFit];
+    [self.titleLabel sizeToFit];
+    
+    switch (self.layoutType) {
+        case ZXGLayoutButtonTypeImageLeftTitleRight:
+            [self layoutHorizontalWithLeftView:self.imageView rightView:self.titleLabel];
+            break;
+        case ZXGLayoutButtonTypeImageRightTitleLeft:
+            [self layoutHorizontalWithLeftView:self.titleLabel rightView:self.imageView];
+            break;
+        case ZXGLayoutButtonTypeImageTopTitleBottom:
+            [self layoutVerticalWithUpView:self.imageView downView:self.titleLabel];
+            break;
+        case ZXGLayoutButtonTypeImageBottomTitleTop:
+            [self layoutVerticalWithUpView:self.titleLabel downView:self.imageView];
+            break;
+        default:
+            break;
+    }
+}
+
+#pragma mark - Initial
+- (void)prepare {
+    _margin = 8.f;
+    _layoutType = ZXGLayoutButtonTypeImageLeftTitleRight;
+}
+
+#pragma mark - Private
+//水平
+- (void)layoutHorizontalWithLeftView:(UIView *)leftView rightView:(UIView *)rightView {
+    CGRect leftViewFrame = leftView.frame;
+    CGRect rightViewFrame = rightView.frame;
+    
+    CGFloat totalWidth = CGRectGetWidth(leftViewFrame) + _margin + CGRectGetWidth(rightViewFrame);
+    
+    leftViewFrame.origin.x = (CGRectGetWidth(self.frame) - totalWidth) * 0.5;
+    leftViewFrame.origin.y = (CGRectGetHeight(self.frame) - CGRectGetHeight(leftViewFrame)) * 0.5;
+    leftView.frame = leftViewFrame;
+    
+    rightViewFrame.origin.x = CGRectGetMaxX(leftViewFrame) + _margin;
+    rightViewFrame.origin.y = (CGRectGetHeight(self.frame) - CGRectGetHeight(rightViewFrame)) * 0.5;
+    rightView.frame = rightViewFrame;
+}
+
+//垂直
+- (void)layoutVerticalWithUpView:(UIView *)upView downView:(UIView *)downView {
+    CGRect upViewFrame = upView.frame;
+    CGRect downViewFrame = downView.frame;
+    
+    CGFloat totalHeight = CGRectGetHeight(upViewFrame) + _margin + CGRectGetHeight(downViewFrame);
+    
+    upViewFrame.origin.y = (CGRectGetHeight(self.frame) - totalHeight) * 0.5;
+    upViewFrame.origin.x = (CGRectGetWidth(self.frame) - CGRectGetWidth(upViewFrame)) * 0.5;
+    upView.frame = upViewFrame;
+    
+    downViewFrame.origin.y = CGRectGetMaxY(upViewFrame) + _margin;
+    downViewFrame.origin.x = (CGRectGetWidth(self.frame) - CGRectGetWidth(downViewFrame)) * 0.5;
+    downView.frame = downViewFrame;
+}
 
 @end
