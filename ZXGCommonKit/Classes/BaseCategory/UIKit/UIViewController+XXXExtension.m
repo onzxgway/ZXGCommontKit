d1d460ae5ea716fa48884b73cf252b1c635045d2
朱献国 <zhuxianguo529@163.com>
2019-05-29 23:03:38 +0800

    Update UIViewController+XXXExtension.m

--- ZXGCommonKit/Classes/BaseCategory/UIKit/UIViewController+XXXExtension.m
+++ ZXGCommonKit/Classes/BaseCategory/UIKit/UIViewController+XXXExtension.m
@@ -10,10 +10,13 @@
 
 @implementation UIViewController (XXXExtension)
 
-//从磁盘加载到内存的时候执行
+//从磁盘加载到内存的时候执行  在main函数之前
 + (void)load {
+    
+    // 单例原理：通过判断静态区的long类型的变量，当为零的时候，执行block，非零的时候，不执行block。
     static dispatch_once_t onceToken;
     dispatch_once(&onceToken, ^{
+        // 这里的代码永远只可能执行一次，因为一旦执行block，onceToken的值就会变为非零。
         Method method1 = class_getInstanceMethod(self, NSSelectorFromString(@"dealloc"));
         Method method2 = class_getInstanceMethod(self, @selector(my_dealloc));
         method_exchangeImplementations(method1, method2);
